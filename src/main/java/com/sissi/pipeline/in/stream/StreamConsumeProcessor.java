763a43f142d6ae67f42d1dd33522c31b256edff0
kim.shen <sjw_job@126.com>
2013-12-03 15:05:40 +0800

    Add stream consume function

---
+++ src/main/java/com/sissi/pipeline/in/stream/StreamConsumeProcessor.java
@@ -0,0 +1,19 @@
+package com.sissi.pipeline.in.stream;
+
+import com.sissi.context.JIDContext;
+import com.sissi.pipeline.Input;
+import com.sissi.protocol.Protocol;
+import com.sissi.protocol.Stream;
+
+/**
+ * @author kim 2013年12月3日
+ */
+public class StreamConsumeProcessor implements Input {
+
+	@Override
+	public Boolean input(JIDContext context, Protocol protocol) {
+		Stream.class.cast(protocol).consume();
+		return true;
+	}
+
+}
