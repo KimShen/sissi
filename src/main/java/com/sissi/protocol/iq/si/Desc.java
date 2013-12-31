60d32cf8b009728fd30a7ba8872645e53cb00ef2
kim.shen <sjw_job@126.com>
2013-12-31 17:04:38 +0800

    Prepare factor
    Signed-off-by: kim.shen <sjw_job@126.com>

---
+++ src/main/java/com/sissi/protocol/iq/si/Desc.java
@@ -0,0 +1,28 @@
+package com.sissi.protocol.iq.si;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlValue;
+
+import com.sissi.read.MappingMetadata;
+
+/**
+ * @author kim 2013年12月31日
+ */
+@MappingMetadata(uri = File.XMLNS, localName = Desc.NAME)
+@XmlRootElement
+public class Desc {
+
+	public final static String NAME = "desc";
+
+	private String text;
+
+	@XmlValue
+	public String getText() {
+		return text;
+	}
+
+	public Desc setText(String text) {
+		this.text = text;
+		return this;
+	}
+}
--- src/main/java/com/sissi/protocol/iq/si/File.java
+++ src/main/java/com/sissi/protocol/iq/si/File.java
@@ -1,8 +1,10 @@
 package com.sissi.protocol.iq.si;
 
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.sissi.read.Collector;
 import com.sissi.read.MappingMetadata;
 
 /**
@@ -10,15 +12,22 @@ import com.sissi.read.MappingMetadata;
  */
 @MappingMetadata(uri = File.XMLNS, localName = File.NAME)
 @XmlRootElement
-public class File {
+public class File implements Collector {
 
 	public final static String XMLNS = "http://jabber.org/protocol/si/profile/file-transfer";
 
 	public final static String NAME = "file";
-	
+
 	private String name;
 
 	private String size;
+
+	private Desc desc;
+
+	@XmlElement
+	public Desc getDesc() {
+		return desc;
+	}
 
 	@XmlAttribute
 	public String getName() {
@@ -43,5 +52,10 @@ public class File {
 	@XmlAttribute
 	public String getXmlns() {
 		return XMLNS;
+	}
+
+	@Override
+	public void set(String localName, Object ob) {
+		this.desc = Desc.class.cast(ob);
 	}
 }
