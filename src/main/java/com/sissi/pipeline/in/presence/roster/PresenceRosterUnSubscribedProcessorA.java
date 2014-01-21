c7480f60f8dd9504972153354b4cc6b4c15b5f82
kim.shen <sjw_job@126.com>
2014-01-21 19:57:59 +0800

    Rename

--- src/main/java/com/sissi/pipeline/in/presence/roster/PresenceRosterUnSubscribedProcessorA.java
+++ src/main/java/com/sissi/pipeline/in/presence/roster/PresenceRosterUnSubscribedProcessorA.java
@@ -8,11 +8,11 @@ import com.sissi.protocol.iq.roster.RosterSubscription;
 /**
  * @author kim 2013-11-17
  */
-public class PresenceRosterUnSubscribedProcessor extends ProxyProcessor {
+public class PresenceRosterUnSubscribedProcessorA extends ProxyProcessor {
 
 	@Override
 	public Boolean input(JIDContext context, Protocol protocol) {
 		super.update(super.build(protocol.getTo()), context.getJid(), RosterSubscription.NONE.toString());
 		return true;
-	} 
+	}
 }
