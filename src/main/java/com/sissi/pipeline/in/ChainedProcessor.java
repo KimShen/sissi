275cf29675f6f881528f1690c0572b9728e22691
kim.shen <sjw_job@126.com>
2014-06-08 02:38:32 +0800

    出现任何异常(Exception)则ChainedProcessor自动终止XMPP流

--- src/main/java/com/sissi/pipeline/in/ChainedProcessor.java
+++ src/main/java/com/sissi/pipeline/in/ChainedProcessor.java
@@ -2,17 +2,29 @@ package com.sissi.pipeline.in;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.sissi.commons.Trace;
 import com.sissi.context.JIDContext;
 import com.sissi.pipeline.Input;
+import com.sissi.protocol.Error;
 import com.sissi.protocol.Protocol;
+import com.sissi.protocol.Stream;
+import com.sissi.protocol.error.ServerError;
+import com.sissi.protocol.error.detail.BadRequest;
 
 /**
  * @author kim 2013-11-14
  */
 public class ChainedProcessor implements Input {
 
+	private final static Error error = new ServerError().add(BadRequest.DETAIL);
+
+	private final static Log log = LogFactory.getLog(ChainedProcessor.class);
+
 	private final List<Input> processors;
-	
+
 	protected final boolean next;
 
 	public ChainedProcessor(List<Input> processors) {
@@ -31,10 +43,16 @@ public class ChainedProcessor implements Input {
 
 	@Override
 	public boolean input(JIDContext context, Protocol protocol) {
-		for (Input each : this.processors) {
-			if (!each.input(context, protocol)) {
-				return false;
+		try {
+			for (Input each : this.processors) {
+				if (!each.input(context, protocol)) {
+					return false;
+				}
 			}
+		} catch (Exception e) {
+			log.warn(e.toString());
+			Trace.trace(log, e);
+			context.write(Stream.closeWhenRunning(error));
 		}
 		return this.next;
 	}
